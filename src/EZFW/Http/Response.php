64c1b1a6fed744038f1583b6685681ebe21d8ceb
Rabrennie <rennierab@gmail.com>
2020-06-22 22:15:56 +0000

    Add text and notFound methods to response class
    text method will return plain text as a response
    
    notFound will return a 404

--- src/EZFW/Http/Response.php
+++ src/EZFW/Http/Response.php
@@ -5,9 +5,24 @@ namespace EZFW\Http;
 class Response {
 
     public string $body = "";
+    public int $response_code = 200;
 
     public function send()
     {
+        http_response_code($this->response_code);
         echo $this->body;
+    }
+
+    public function text(string $body)
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    public function notFound(string $body)
+    {
+        $this->response_code = 404;
+        $this->body = $body;
+        return $this;
     }
 }
