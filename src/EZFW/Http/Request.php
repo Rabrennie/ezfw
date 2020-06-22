ed1021e75184c56cc427c848457b5b467b985ed2
Rabrennie <rennierab@gmail.com>
2020-06-22 22:15:06 +0000

    Update request class to parse method, and URI

--- src/EZFW/Http/Request.php
+++ src/EZFW/Http/Request.php
@@ -3,6 +3,19 @@
 namespace EZFW\Http;
 
 class Request {
+
+    public string $method = 'GET';
+    public string $path = '/';
+
+    public function __construct()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        
+        $parsed = parse_url($_SERVER['REQUEST_URI']);
+        $this->path = $parsed['path'];
+        $this->query = $parsed['query'];
+    }
+
     public static function current() : Request
     {
         return new self();
