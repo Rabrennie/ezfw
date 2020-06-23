002a19e4ae90dcbf905bfdc5132be9b48d896b80
Rabrennie <rennierab@gmail.com>
2020-06-23 21:30:36 +0000

    Update response to be able to set headers

--- src/EZFW/Http/Response.php
+++ src/EZFW/Http/Response.php
@@ -6,15 +6,27 @@ class Response {
 
     public string $body = "";
     public int $response_code = 200;
+    public array $headers = [];
 
     public function send()
     {
+        foreach($this->headers as $key => $value) {
+            header("{$key}: {$value}");
+        }
         http_response_code($this->response_code);
         echo $this->body;
     }
 
     public function text(string $body)
     {
+        $this->addHeader('Content-type', 'text/plain; charset=UTF-8');
+        $this->body = $body;
+        return $this;
+    }
+
+    public function html(string $body)
+    {
+        $this->addHeader('Content-type', 'text/html; charset=UTF-8');
         $this->body = $body;
         return $this;
     }
@@ -24,5 +36,10 @@ class Response {
         $this->response_code = 404;
         $this->body = $body;
         return $this;
+    }
+
+    public function addHeader(string $key, string $value)
+    {
+        $this->headers[$key] = $value;
     }
 }
