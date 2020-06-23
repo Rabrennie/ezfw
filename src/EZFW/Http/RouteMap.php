9f199448b5976e6ee7613e0717a104c547daf576
Rabrennie <rennierab@gmail.com>
2020-06-23 00:06:11 +0000

    Update router to use tree structure and handle route parameters

---
+++ src/EZFW/Http/RouteMap.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace EZFW\Http;
+
+class RouteMap {
+    public array $children = [];
+    public string $routePart = '';
+    public bool $isParameter = false;
+    public $callback;
+
+    public function __construct(string $routePart)
+    {
+        $re = '/{(.*)}/m';
+        preg_match_all($re, $routePart, $matches, PREG_SET_ORDER, 0);
+        if (count($matches) > 0) {
+            $this->isParameter = true;
+            $this->routePart = $matches[0][1];
+        } else {
+            $this->routePart = $routePart;
+        }
+    }
+
+    public function addChild(RouteMap $route)
+    {
+        $this->children[$route->routePart] = $route;
+    }
+}
--- src/EZFW/Http/Router.php
+++ src/EZFW/Http/Router.php
@@ -6,15 +6,75 @@ class Router
 {
     public const METHOD_GET = "GET";
 
-    public $GET = [];
+    public $routeMap;
+
+    public function __construct()
+    {
+        $this->routeMap = new RouteMap('/');
+    }
 
     public function add(string $method, string $route, callable $callback)
     {
-        $this->$method[$route] = $callback;
+        // TODO: refactor and support different methods
+        $routeParts = ['/', ...preg_split('/\//', $route, null, PREG_SPLIT_NO_EMPTY)];
+
+        $currentRouteMap = $this->routeMap;
+
+        for ($i=0; $i < count($routeParts); $i++) { 
+            $current = $routeParts[$i];
+
+            $found = false;
+
+            foreach ($currentRouteMap->children as $child) {
+                if ($child->isParameter || $child->routePart == $current) {
+                    $currentRouteMap = $child;
+                    $found = true;
+                    break;
+                }
+            }
+
+            if (!$found) {
+                $temp = new RouteMap($current);
+                $currentRouteMap->addChild($temp);
+                $currentRouteMap = $temp;
+            }
+
+            if (($currentRouteMap->isParameter || $currentRouteMap->routePart == $current) && $i == count($routeParts) - 1) {
+                $currentRouteMap->callback = $callback;
+                break;
+            }
+        }
     }
 
     public function resolve(Request $request)
     {
-        return $this->{$request->method}[$request->path];
+        $routeParts = ['/', ...preg_split('/\//', $request->path, null, PREG_SPLIT_NO_EMPTY)];
+
+        $currentRouteMap = $this->routeMap;
+
+        for ($i=0; $i < count($routeParts); $i++) { 
+            $current = $routeParts[$i];
+
+            $found = false;
+
+            foreach ($currentRouteMap->children as $child) {
+                if ($child->isParameter) {
+                    $request->setParameter($child->routePart, $current);
+                }
+
+                if ($child->isParameter || $child->routePart == $current) {
+                    $currentRouteMap = $child;
+                    $found = true;
+                    break;
+                }
+            }
+
+            if (!$found) {
+                return null;
+            }
+
+        }
+
+        return $currentRouteMap->callback;
     }
 }
